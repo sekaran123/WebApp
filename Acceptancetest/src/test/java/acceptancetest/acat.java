cfd541bb2c92337a6da927171768760f434b24f8
sekaran123 <38779213+sekaran123@users.noreply.github.com>
2018-05-04 10:06:47 +0530

    Update acat.java

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -15,7 +15,7 @@ public class acat {
 	//public String URL = "http://52.77.237.142:8080/ProdWebapp";
 	//public String URL1 = "http://52.77.237.142:8080/ProdWebapp/avncreatepage.jsp";
 	public String URL = "http://18.191.47.214:8080/ProdWebapp";
-	public String URL1 = "http://18.191.47.214:8080/ProdWebapp/avncreatepage.jsp"
+	public String URL1 = "http://18.191.47.214:8080/ProdWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	
